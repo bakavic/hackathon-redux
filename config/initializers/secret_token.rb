346dead80fa00e4b3795f4eea2ff48b1e8bacc2b
Victor Heng <bakavic@gmail.com>
2012-02-20 00:08:23 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackathonRedux::Application.config.secret_token = '5f0726ec0598cdb44b066769751a2b905ff88fbb0cdbf0fffd46b604bb708622fccc1b0d1168cc60a7c4afae42b3e8a6446c0478b4ed6c6b0e9edee727f19e9e'
